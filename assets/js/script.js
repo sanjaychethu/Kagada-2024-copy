f2fdc046cbdf9e9a5c56362b33b9c50bfe2ea1a4
sanjaychethu <chethuhustler@gmail.com>
2024-10-09 01:05:38 +0530

    scroll-button & smooth animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,27 @@ navLinkItems.forEach(link => {
         closeMenu();
     });
 });
+
+
+// Get the button
+let myButton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    myButton.classList.add("show");
+  } else {
+    myButton.classList.remove("show");
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
